6f870bd0cd249911fb084392854018ee3e0a8d04
alisa <ALISA0710@163.com>
2017-11-17 15:36:12 +0800

    数据可视化--本地版本

---
+++ js/reset.js
@@ -0,0 +1,13 @@
+﻿(function(doc, win){
+			var docEl = doc.documentElement, 
+			resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize', recalc = function(){
+				var clientWidth = docEl.clientWidth;
+				docEl.style.fontSize = 100* (clientWidth / 1920) + 'px';
+			};
+			if(!doc.addEventListener){
+				return;
+			}
+			win.addEventListener(resizeEvt, recalc, false);
+			doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
